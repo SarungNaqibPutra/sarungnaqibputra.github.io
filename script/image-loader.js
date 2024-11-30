5aff6a83fddb6958a1377b00ab27eb884a3742df
SarungNaqibPutra <naqibsarung@gmail.com>
2024-12-01 04:53:20 +0700

    remove image loader

--- script/image-loader.js
+++
@@ -1,9 +0,0 @@
-function imageLoader () {
-    fetch('/page/imageloader.html')
-    .then(Response => Response.text())
-    .then(html => {
-        const loadContainer = document.getElementsByClassName('image-loader')
-        imageLoader.innerHTML = html;
-    });
-}
-navbarLoader();
